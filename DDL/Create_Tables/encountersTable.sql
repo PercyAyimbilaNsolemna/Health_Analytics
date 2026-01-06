113d386ef828b6b6b4e3f8b3c3d24f0694919023
PercyAyimbilaNsolemna <percy.nsolebna@gmail.com>
2026-01-06 00:09:46 +0000

    Removed the semicolon from the create encounters table script and changed the comment to the right comment format.

--- DDL/Create_Tables/encountersTable.sql
+++ DDL/Create_Tables/encountersTable.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS encounters (
   encounter_id INT PRIMARY KEY,
   patient_id INT,
   provider_id INT,
-  encounter_type VARCHAR (50), -- 'Outpatient', 'Inpatient', 'ER'
+  encounter_type VARCHAR (50), /* 'Outpatient', 'Inpatient', 'ER' */
   encounter_date DATETIME,
   discharge_date DATETIME,
   department_id INT,
@@ -10,4 +10,4 @@ CREATE TABLE IF NOT EXISTS encounters (
   FOREIGN KEY (provider_id) REFERENCES providers (provider_id),
   FOREIGN KEY (department_id) REFERENCES departments (department_id),
   INDEX idx_encounter_date (encounter_date)
-);
+)
