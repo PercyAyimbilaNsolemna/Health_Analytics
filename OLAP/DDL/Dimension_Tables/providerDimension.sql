d301b7f40592a3f04138aa8990d7e91ea588dbd3
PercyAyimbilaNsolemna <percy.nsolebna@gmail.com>
2026-01-25 00:35:32 +0000

    Removed department_key and specialty_key from the provider dimention table.

--- OLAP/DDL/Dimension_Tables/providerDimension.sql
+++ OLAP/DDL/Dimension_Tables/providerDimension.sql
@@ -3,7 +3,5 @@ CREATE TABLE IF NOT EXISTS dim_provider (
     provider_id INT NOT NULL,
     provider_name VARCHAR(200),
     credential VARCHAR(20),
-    specialty_key INT,
-    department_key INT,
     UNIQUE (provider_id)
 )
